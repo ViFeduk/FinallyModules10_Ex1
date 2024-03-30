c756d162d3d699b6728ba76251783f09d475502a
ViFeduk <viktor2005fedchenkov@mail.ru>
2024-03-30 15:34:31 +0300

    Добавьте файлы проекта.

---
+++ FinallyModules10_Ex1/Colculater.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinallyModules10_Ex1
+{
+    internal class Colculater : IColCulate
+    {
+        public double Colculate(double first, double second)
+        {
+           
+            return first + second;
+            
+        }
+
+        public void WorkCalculate()
+        {
+            double first;
+            double second;
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Введите два числа для сложения");
+                    first = Convert.ToDouble(Console.ReadLine());
+                    second = Convert.ToDouble(Console.ReadLine());
+
+                    Console.WriteLine($"Сумма этих числе равна: {Colculate(first,second)}");
+                }
+                catch
+                {
+                    Console.WriteLine("Введите корректные значения");
+                }
+            }
+        }
+    }
+}
---
+++ FinallyModules10_Ex1/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace FinallyModules10_Ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var colculater = new Colculater();
+            colculater.WorkCalculate();
+        }
+
+    }
+}
